4b7bf37e07672bcec85bca124a77ca23bda5ec10
Miroslavetsh <dfgshte@gmail.com>
2021-02-09 18:23:08 +0200

    Fixed scroll disability

--- src/main/java/org/obrii/mit/dp2021/toloshnyi/dp2021project/User.java
+++ src/main/java/org/obrii/mit/dp2021/toloshnyi/dp2021project/User.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.obrii.mit.dp2021.toloshnyi.dp2021project;
 
 import java.util.Arrays;
 import java.util.List;
 
-/**
- *
- * @author gxufh
- */
 public class User {
     
     
