b586845a0c0b304b1d6015ed7f81cdb97a9ae764
Miroslavetsh <dfgshte@gmail.com>
2021-02-11 16:09:39 +0200

    Initial commit

---
+++ src/main/java/org/obrii/mit/dp2021/toloshnyi/toloshnyilab2/user/User.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.obrii.mit.dp2021.toloshnyi.toloshnyilab2.user;
+
+/**
+ *
+ * @author gxufh
+ */
+public class User implements OldInterface {
+    public String name;
+    
+    public User(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
