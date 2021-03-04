915192916676f76c1c0290738efcf6458abc7661
Miroslavetsh <dfgshte@gmail.com>
2021-03-04 09:43:24 +0200

    Added css, js, filter and too much

--- src/main/java/org/obrii/mit/dp2021/toloshnyi/toloshnyilab3/servlets/DataStorageRefactorerInterface.java
+++ src/main/java/org/obrii/mit/dp2021/toloshnyi/toloshnyilab3/servlets/DataStorageRefactorerInterface.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package org.obrii.mit.dp2021.toloshnyi.toloshnyilab3.data;
-
 import java.util.List;
+import org.obrii.mit.dp2021.toloshnyi.toloshnyilab3.data.Data;
 
 /**
  *
